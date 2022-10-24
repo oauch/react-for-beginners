8068701965b0a315a2f94d56d9f30cec3434af03
oauch <lgm9515@naver.com>
2022-10-24 13:24:18 +0900

    finish

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,32 +1,37 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import React from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
-function Home(){
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    );
-    const json = await response.json();
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
   useEffect(() => {
     getMovies();
   }, []);
-  console.log(movies);
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
+    <div className={styles.container}>
+    {loading ? (
+      <div className={styles.loader}>
+      <span>Loading...</span>
+    </div>
+  ) : (
+    <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
-              coverImg={movie.medium_cover_image}
+              key={movie.id}
               id={movie.id}
+              year={movie.year}
+              coverImg={movie.medium_cover_image}
               title={movie.title}
               summary={movie.summary}
               genres={movie.genres}
@@ -37,5 +42,4 @@ function Home(){
     </div>
   );
 }
-
-export default Home
+export default Home;
